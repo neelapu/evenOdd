e048fd481a8a8d751de43224e3b8e814b7d98baf
neelapu <raj.neelapu@gmail.com>
2018-07-15 14:01:38 +0530

    EvenOdd

---
+++ script.js
@@ -0,0 +1,51 @@
+var count = 20;
+var numsEven = [];
+var numsOdd = [];
+var setInt;
+var startGame = false;
+var button = document.getElementById("startCount");
+   
+
+function startEvenOdd() {
+  startGame = true;
+  
+  if(startGame){
+    setInt = setInterval(goFun, 1000);
+    button.textContent = 'Wait';
+    }
+}
+
+function goFun() {
+  //--count;
+  document.getElementById("numbers").textContent = --count;
+  
+  if(count % 2 === 0) {
+    numsEven.push(count);
+    //document.getElementById("evenNum").textContent = numsEven;
+  }
+  
+  if(Math.abs(count % 2) === 1) {
+     numsOdd.push(count);
+     //document.getElementById("oddNum").textContent = numsOdd;
+  }
+    
+  for(var x of numsEven){
+     document.getElementById("evenNum").textContent = x;
+  };
+  for(var y of numsOdd){
+     document.getElementById("oddNum").textContent = y;
+  };
+  
+  if(count === 0){
+    
+    clearInterval(setInt);
+    document.getElementById("numbers").innerHTML = "Hi!";
+    //console.log(numsEven);
+    // console.log(numsOdd);
+    startGame = false;
+    button.textContent = 'Start';
+  }
+  
+}
+
+button.addEventListener('click', startEvenOdd, false);
